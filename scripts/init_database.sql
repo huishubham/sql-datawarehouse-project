0830b0d584eb6bf22e738de7b1ccfdf46ddefd47
Shubham Panwar <panwarshubham9315@gmail.com>
2025-10-02 14:35:01 +0530

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -12,8 +12,27 @@ WARNING:
     and ensure you have proper backups before running this script.
 */
 
--- Drop and recreate the 'DataWarehouse' database
-DROP DATABASE IF EXISTS DataWarehouse;
+USE master;
 
+-- Drop and recreate 'DataWarehouse' Database
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse')
+BEGIN 
+    ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+    DROP DATABASE DataWarehouse;
+END;
+GO
+
+-- Creating Database 'DataWarehouse'
 CREATE DATABASE DataWarehouse;
 
+USE DataWarehouse;
+GO
+
+-- Creating schemas
+CREATE SCHEMA bronze;
+GO
+
+CREATE SCHEMA silver;
+GO
+
+CREATE SCHEMA gold;
